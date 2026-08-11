6c8dd8aa9c52534c28e7cff4555f987b1ac3b049
Claudeilton Dantas <fcd007@hotmail.com>
2026-08-10 22:04:14 -0300

    feat(adicionado recursos load balancer): ajuste lb e parameters

--- load_balancer.tf
+++ load_balancer.tf
@@ -19,6 +19,17 @@ resource "aws_security_group_rule" "ingress_80" {
   security_group_id = aws_security_group.sg_lb.id
   description       = "Allow HTTP traffic from the internet 80"
 }
+
+resource "aws_security_group_rule" "ingress_443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_lb.id
+  description       = "Allow HTTPS traffic from the internet 443"
+}
+
 resource "aws_lb" "main_lb" {
   name               = format("%s-ingress", var.projetct_name)
   internal           = var.load_balancer_internal
@@ -30,12 +41,28 @@ resource "aws_lb" "main_lb" {
     data.aws_ssm_parameter.public_subnet_1c.value
   ]
 
-  security_groups = [aws_security_group.sg_lb.id]
+  security_groups = [
+    aws_security_group.sg_lb.id
+  ]
 
   enable_deletion_protection       = false
   enable_cross_zone_load_balancing = false
 
   tags = {
     Name = format("%s-ingress", var.projetct_name)
+  }
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Linuxtips"
+      status_code  = 200
+    }
   }
 }
